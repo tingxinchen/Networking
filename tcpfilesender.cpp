6f4b95f61f86d92e63fd0da99016761420cf44e8
chentingxin <41343128@nfu.edu.tw>
2025-11-28 19:07:59 +0800

    updata

---
+++ tcpfilesender.cpp
@@ -0,0 +1,85 @@
+#include "tcpfilesender.h"
+
+TcpFileSender::TcpFileSender(QWidget *parent)
+    : QDialog(parent)
+{}
+
+TcpFileSender::~TcpFileSender() {
+    loadSize = 1024*4;
+    totalBytes = 0;
+    bytesWritten = 0;
+    bytesToWrite = 0;
+    clientProgressBar = new QProgressBar;
+    clientStatusLabel = new QLabel (tr("客戶端就緒"));
+    startButton = new QPushButton (tr("開始"));
+    quitButton = new QPushButton(tr("退出"));
+    openButton = new QPushButton (tr("開檔"));
+    startButton->setEnabled(false);
+    buttonBox = new QDialogButtonBox;
+    buttonBox->addButton (startButton, QDialogButtonBox:: ActionRole);
+    buttonBox->addButton (openButton, QDialogButtonBox:: ActionRole);
+    buttonBox->addButton (quitButton, QDialogButtonBox:: RejectRole);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget (clientProgressBar);
+    mainLayout->addWidget (clientStatusLabel);
+    mainLayout->addStretch (1);
+    mainLayout->addSpacing (10);
+    mainLayout->addWidget (buttonBox);
+    setLayout (mainLayout);
+    setWindowTitle (tr("檔案傳送"));
+    connect (openButton, SIGNAL (clicked()), this, SLOT (openFile()));
+    connect (startButton, SIGNAL (clicked()), this, SLOT (start()));
+    connect(&tcpClient, SIGNAL (connected()), this, SLOT (startTransfer()));
+    connect(&tcpClient, SIGNAL (bytesWritten (qint64)), this, SLOT (updateClientProgress (qint64)));
+    connect (quitButton, SIGNAL (clicked()), this, SLOT (close()));
+}
+void TcpFileSender::start()
+{
+    startButton->setEnabled(false);
+    bytesWritten = 0;
+    clientStatusLabel->setText(tr("連接中..."));
+    tcpClient.connectToHost(QHostAddress:: LocalHost, 16689);
+}
+void TcpFileSender::startTransfer()
+{
+    localFile = new QFile(fileName);
+    if (!localFile->open (QFile::ReadOnly))
+    {
+        QMessageBox:: warning(this,tr("應用程式"),
+                             tr("無法讀取 %1:\n%2.").arg(fileName) .arg (localFile->errorString()));
+        return;
+    }
+    totalBytes = localFile->size();
+    QDataStream sendOut (&outBlock, QIODevice::WriteOnly);
+    sendOut.setVersion (QDataStream:: Qt_4_6);
+    QString currentFile = fileName.right (fileName.size()-
+                                         fileName.lastIndexOf("/")-1);
+    sendOut <<qint64(0)<<qint64(0) <<currentFile;
+    totalBytes += outBlock.size();
+
+    sendOut.device ()->seek(0);
+    sendOut<<totalBytes<<qint64((outBlock.size()-sizeof(qint64)*2));
+    bytesToWrite = totalBytes-tcpClient.write(outBlock);
+    clientStatusLabel->setText(tr("已連接"));
+    qDebug() << currentFile <<totalBytes;
+    outBlock.resize(0);
+}
+void TcpFileSender::updateClientProgress (qint64 numBytes)
+{
+    bytesWritten += (int) numBytes;
+    if (bytesToWrite > 0)
+    {
+        outBlock=localFile->read (qMin (bytesToWrite, loadSize));
+        bytesToWrite = (int) tcpClient.write(outBlock);
+        outBlock.resize(0);
+    }else
+    {
+        localFile->close();
+    }
+}
+void TcpFileSender::openFile()
+{
+    fileName = QFileDialog::getOpenFileName(this);
+    if (!fileName.isEmpty()) startButton->setEnabled(true);
+}
---
+++ tcpfilesender.h
@@ -0,0 +1,39 @@
+#ifndef TCPFILESENDER_H
+#define TCPFILESENDER_H
+
+#include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
+class TcpFileSender : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit TcpFileSender(QWidget *parent = nullptr);
+    ~TcpFileSender();
+
+public slots:
+    void start();
+    void startTransfer();
+    void updateClientProgress(qint64 numBytes);
+    void openFile();
+
+private:
+    QProgressBar     *clientProgressBar;
+    QLabel           *clientStatusLabel;
+    QPushButton      *startButton;
+    QPushButton      *quitButton;
+    QPushButton      *openButton;
+    QDialogButtonBox *buttonBox;
+
+    QTcpSocket       tcpClient;
+    qint64           totalBytes;
+    qint64           bytesWritten;
+    qint64           bytesToWrite;
+    qint64           loadSize;
+    QString          fileName;
+    QFile            *localFile;
+    QByteArray       outBlock;
+};
+
+#endif // TCPFILESENDER_H
